290e61f5de752812961572d60e8f7baf1f57a958
Nathan <suprdory@gmail.com>
2021-12-29 02:56:50 +0000

    version 1.1, status bar removed

--- app/src/main/java/com/suprdory/murmur/MainActivity.kt
+++ app/src/main/java/com/suprdory/murmur/MainActivity.kt
@@ -2,15 +2,36 @@ package com.suprdory.murmur
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
 import android.webkit.WebView
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         supportActionBar?.hide()
+
+//        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN)
+//        setContentView(R.layout.activity_main)
+
+//        hideSystemBars()
+
         val myWebView: WebView = findViewById(R.id.webview)
         myWebView.settings.javaScriptEnabled = true
         myWebView.loadUrl("https://suprdory.github.io/murmur/")
     }
+//    private fun hideSystemBars() {
+//        val windowInsetsController = ViewCompat.getWindowInsetsController(window.decorView) ?: return
+//        // Configure the behavior of the hidden system bars
+//        windowInsetsController.systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+//        // Hide both the status bar and the navigation bar
+//        windowInsetsController.hide(WindowInsetsCompat.Type.systemBars())
+//    }
 }
+
